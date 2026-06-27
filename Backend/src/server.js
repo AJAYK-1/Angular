084f4d347d41a6eef22716c31f5faa0bea66177a
AJAYK-1 <ajaykumartp10@gmail.com>
2026-06-27 18:55:35 +0530

    "server setup"

---
+++ Backend/src/server.js
@@ -0,0 +1,10 @@
+import express from 'express'
+import { config } from 'dotenv'
+config()
+
+const app = express()
+app.use(express.json())
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`Server Running on PORT: ${PORT}`)
+)
