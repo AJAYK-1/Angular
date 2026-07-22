8478fa2df54d7d3156ee87dff3d9ced9c5e952e4
AJAYK-1 <ajaykumartp10@gmail.com>
2026-07-22 21:23:50 +0530

    chore: remove Frontend Angular application files and configurations
    - Deleted package.json, index.html, and main.ts files.
    - Removed all Angular components, services, routes, and their respective test files.
    - Cleared out styles and HTML templates for various components.
    - Removed server-side rendering configurations and related files.
    - Cleaned up TypeScript configuration files.

--- Frontend/src/app/practice/api-integration/api-integration.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-api-integration',
-  imports: [],
-  templateUrl: './api-integration.html',
-  styleUrl: './api-integration.css',
-})
-export class ApiIntegration {}
